05a7106fed0142a97714f95d0074c92cd19beda2
Vishal Balaji D <vishalbalaji@arch>
2022-11-12 18:03:30 +0530

    :arrow_up_small: add cmp bindings

--- lua/user/lsp/cmp.lua
+++ lua/user/lsp/cmp.lua
@@ -162,7 +162,9 @@ cmp.setup.cmdline(":", {
 	mapping = {
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+		["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
 		["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+		["<Down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
 		["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
 		["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
 	},
@@ -175,7 +177,9 @@ cmp.setup.cmdline({ "/", "?" }, {
 	mapping = {
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+		["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
 		["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+		["<Down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
 		["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
 		["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
 	},
