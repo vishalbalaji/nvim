6ebe85c054a0e7e84fbf96859c6e22a752ba4cab
Vishal Balaji D <vishalbalaji@arch>
2022-12-20 18:57:27 +0530

    :repeat: minor refactor

---
+++ lua/user/autocommands.lua
@@ -0,0 +1,39 @@
+local autocmd = vim.api.nvim_create_autocmd
+local kitty_group = vim.api.nvim_create_augroup("kitty_mp", {
+	clear = true,
+})
+
+autocmd("FileType", {
+	once = true,
+	pattern = { "help", "qf" },
+	command = [[ map q :quit<CR> ]],
+})
+
+autocmd("BufRead", {
+	callback = function()
+		autocmd("BufWinEnter", {
+			once = true,
+			command = "normal! zxzR",
+		})
+	end,
+})
+
+autocmd({ "VimEnter" }, {
+	once = true,
+	pattern = { vim.env.MYVIMRC, "zshrc" },
+	command = [[ cd %:p:h ]],
+})
+
+autocmd({ "VimEnter" }, {
+	once = true,
+	pattern = "*",
+	group = kitty_group,
+	command = [[ silent ![ "$TERM" = "xterm-kitty" ] &&  kitty @ --to=$KITTY_LISTEN_ON set-spacing padding=0 ]],
+})
+
+autocmd({ "VimLeavePre" }, {
+	once = true,
+	pattern = "*",
+	group = kitty_group,
+	command = [[ silent ![ "$TERM" = "xterm-kitty" ] &&  kitty @ --to=$KITTY_LISTEN_ON set-spacing padding=10 ]],
+})
--- lua/user/set.lua
+++ lua/user/set.lua
@@ -22,7 +22,7 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = false
 
-vim.opt.wrap = true
+vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
