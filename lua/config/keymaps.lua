c3b6d688d38c3453b13c843b54b4babb22e71644
Vishal Balaji D <vishalbalaji@arch>
2023-01-05 17:37:11 +0530

    :bug: fix crappy c and d mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -32,58 +32,14 @@ map("n", "n", "nzzzv")
 map("n", "N", "Nzzzv")
 
 -- Clipboard Stuff
--- -- Function to delete in range, such as di(, da{, etc. without overwriting clipboard
--- -- CREDIT: https://stackoverflow.com/a/72197874
-function _G.range_delete(startinsert)
-	startinsert = startinsert or false
-	local old_func = vim.go.operatorfunc -- backup previous reference
-	-- set a globally callable object/function
-	_G.op_func_delete = function()
-		-- the content covered by the motion is between the [ and ] marks, so get those
-		local start = vim.api.nvim_buf_get_mark(0, "[")
-		local finish = vim.api.nvim_buf_get_mark(0, "]")
-
-		if finish[1] == start[1] then
-			finish[1] = finish[1] - 1
-			finish[2] = finish[2] + 1
-		end
-
-		start[1] = start[1] - 1
-
-		vim.api.nvim_buf_set_text(0, start[1], start[2], finish[1], finish[2], {})
-		if startinsert then
-			vim.api.nvim_feedkeys("i", "n", false)
-		end
-
-		vim.go.operatorfunc = old_func
-		_G.op_func_delete = nil -- deletes itself from global namespace
+local function better_delete_func(key)
+	local action = vim.fn.nr2char(vim.fn.getchar())
+	local operator = ""
+	if action ~= key then
+		operator = vim.fn.nr2char(vim.fn.getchar())
 	end
-	vim.go.operatorfunc = "v:lua.op_func_delete"
-	vim.api.nvim_feedkeys("g@", "n", false)
+	vim.api.nvim_feedkeys('"_' .. key .. action .. operator, "n", false)
 end
-
-local function delete_func(key, startinsert)
-	startinsert = startinsert or false
-	local operator = vim.fn.nr2char(vim.fn.getchar())
-
-	if operator == key then
-		vim.api.nvim_feedkeys('"_' .. key .. key, "n", false)
-	else
-		range_delete(startinsert)
-		vim.api.nvim_feedkeys(operator, "n", false)
-	end
-end
-
--- local function better_delete_func(key)
--- 	local operator = vim.fn.nr2char(vim.fn.getchar())
-
--- 	if operator == key then
--- 		vim.api.nvim_feedkeys('"_' .. key .. key, "n", false)
--- 	else
--- 		vim.api.nvim_feedkeys(vim.fn.nr2char(vim.fn.getchar()), "n", false)
--- 		vim.api.nvim_feedkeys(vim.fn.nr2char(vim.fn.getchar()), "n", false)
--- 	end
--- end
 
 map("v", "p", '"_dP')
 map("v", "d", '"_d')
@@ -94,12 +50,12 @@ map("x", "r", '"_r')
 
 map("n", "xx", "Vd")
 
--- map("n", "d", function()
--- 	better_delete_func("d")
--- end)
--- map("n", "c", function()
--- 	better_delete_func("c")
--- end)
+map("n", "d", function()
+	better_delete_func("d")
+end)
+map("n", "c", function()
+	better_delete_func("c")
+end)
 
 -- Move
 map("n", "<A-h>", "<Plug>GoNSMLeft")
