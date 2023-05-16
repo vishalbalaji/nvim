5ac4885bf4e47fe6efddc7c0883cf59bf09ef6ab
vishalbalaji <vishalbalaji@gmail.com>
2023-05-16 12:29:43 +0530

    :arrow_up_small: fix neo-tree nerd fonts icons

--- lua/config/plugins/neo-tree.lua
+++ lua/config/plugins/neo-tree.lua
@@ -50,6 +50,40 @@ M.config = function()
 	vim.g.neo_tree_remove_legacy_commands = 1
 
 	require("neo-tree").setup({
+		-- BEGIN NerdFonts V3 fix --
+		default_component_configs = {
+			icon = {
+				folder_empty = "󰜌",
+				folder_empty_open = "󰜌",
+			},
+			git_status = {
+				symbols = {
+					renamed = "󰁕",
+					unstaged = "󰄱",
+				},
+			},
+		},
+		document_symbols = {
+			kinds = {
+				File = { icon = "󰈙", hl = "Tag" },
+				Namespace = { icon = "󰌗", hl = "Include" },
+				Package = { icon = "󰏖", hl = "Label" },
+				Class = { icon = "󰌗", hl = "Include" },
+				Property = { icon = "󰆧", hl = "@property" },
+				Enum = { icon = "󰒻", hl = "@number" },
+				Function = { icon = "󰊕", hl = "Function" },
+				String = { icon = "󰀬", hl = "String" },
+				Number = { icon = "󰎠", hl = "Number" },
+				Array = { icon = "󰅪", hl = "Type" },
+				Object = { icon = "󰅩", hl = "Type" },
+				Key = { icon = "󰌋", hl = "" },
+				Struct = { icon = "󰌗", hl = "Type" },
+				Operator = { icon = "󰆕", hl = "Operator" },
+				TypeParameter = { icon = "󰊄", hl = "Type" },
+				StaticMethod = { icon = "󰠄 ", hl = "Function" },
+			},
+		},
+		-- END NerdFonts V3 fix --
 		filesystem = {
 			-- hijack_netrw_behavior = "open_default",
 			hijack_netrw_behavior = "open_current",
