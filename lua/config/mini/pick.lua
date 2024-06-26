80bf8a1aaa9535f0f7c4ae895accef124dd3858e
vishalbalaji <vishalbalaji@gmail.com>
2024-06-27 02:53:50 +0530

    :sparkles: make mini.pick read hidden files

--- lua/config/mini/pick.lua
+++ lua/config/mini/pick.lua
@@ -1,25 +1,27 @@
 return {
 	"echasnovski/mini.pick",
 	version = "*",
+	dependencies = {
+		require("config.devicons"),
+	},
 	cmd = "Pick",
 	keys = {
 		{
 			"<leader>ff",
 			function()
-				-- MiniPick.builtin.files({}, { source = { cwd = vim.fn.expand("%:p:h") } })
 				vim.cmd.Pick("files")
 			end,
 		},
 		{
 			"<leader>fF",
 			function()
-				MiniPick.builtin.grep_live()
+				vim.cmd.Pick("grep_live")
 			end,
 		},
 		{
 			"<leader>fh",
 			function()
-				MiniPick.builtin.help()
+				vim.cmd.Pick("help")
 			end,
 		},
 	},
@@ -32,8 +34,7 @@ return {
 	config = function(_, opts)
 		require("mini.pick").setup(opts)
 		MiniPick.registry.files = function(local_opts)
-			local command =
-				{ "fd", "--type=f", "--no-follow", "--color=never", "--hidden", "-E.git", "-Enode_modules" }
+			local command = { "fd", "--type=f", "--no-follow", "--color=never", "--hidden", "-E.git", "-Enode_modules" }
 			local show_with_icons = function(buf_id, items, query)
 				return MiniPick.default_show(buf_id, items, query, { show_icons = true })
 			end
@@ -42,7 +43,4 @@ return {
 			return MiniPick.builtin.cli({ command = command }, { source = source })
 		end
 	end,
-	dependencies = {
-		require("config.devicons"),
-	},
 }
